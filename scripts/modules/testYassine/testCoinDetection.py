02525862a99bc7180cf746b66c1bb6dd040743ae
Yassine <yassinefekih78@outlook.fr>
2024-03-24 18:46:29 +0100

    hough?

--- scripts/modules/testYassine/testCoinDetection.py
+++ scripts/modules/testYassine/testCoinDetection.py
@@ -37,7 +37,8 @@ def otsu_thresholding(channel):
     return thresh
 
 def circular_hough_transform(image, radius_range, search_threshold):
-    circles = cv.HoughCircles(image, cv.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=30, minRadius=radius_range[0], maxRadius=radius_range[1])
+    rows = image.shape[0]
+    circles = cv.HoughCircles(image, cv.HOUGH_GRADIENT, 1, rows / 8)
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
@@ -47,57 +48,109 @@ def circular_hough_transform(image, radius_range, search_threshold):
 
     return circles
 
-image = cv.imread("/home/yassfkh/Desktop/ProjetImage/ProjetImage/Images/275.png", 1)
+
+def apply_closing(image, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    closing = cv.morphologyEx(image, cv.MORPH_CLOSE, kernel)
+    return closing
+
+def apply_opening(image, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    opening = cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
+    return opening
+
+
+def normalize_image(image):
+    normalized_image = np.zeros(image.shape, np.uint8)
+    cv.normalize(image, normalized_image, 0, 255, cv.NORM_MINMAX)
+    return normalized_image
+
+
+image = cv.imread("/home/yassfkh/Desktop/ProjetImage/ProjetImage/Images/211.jpg", 1)
 
 
 
 resized_image = resize_image(image, 400, 400)
 greyImage = RGB_to_greyscale(resized_image)
-blurredImage = blur_image(greyImage)
-
-# NORMALISER APRES LE FLOU ET AVANT LAPLACE ?
-
-
+normalized_blurredImage = normalize_image(greyImage)
+blurredImage = blur_image(normalized_blurredImage)
 laplaceImage = laplace_filter(blurredImage)
 thresholdedImage = otsu_thresholding(laplaceImage)
 thresholdedImageBeforeLaplace = otsu_thresholding(blurredImage)
 
-plt.figure(figsize=(12, 8))
+closedImage = apply_closing(thresholdedImage, kernel_size=6)
+openImage = apply_opening(thresholdedImage, kernel_size=6)
 
-plt.subplot(3, 3, 1)
+plt.figure(figsize=(18, 12))
+
+plt.subplot(3, 4, 1)
 plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
 plt.title("Image originale")
 plt.axis('off')
 
-plt.subplot(2, 3, 2)
+plt.subplot(3, 4, 2)
 plt.imshow(cv.cvtColor(resized_image, cv.COLOR_BGR2RGB))
 plt.title("Resized image")
 plt.axis('off')
 
-plt.subplot(2, 3, 3)
+plt.subplot(3, 4, 3)
 plt.imshow(greyImage, cmap='gray')
 plt.title("Greyscale image")
 plt.axis('off')
 
-plt.subplot(2, 3, 4)
+plt.subplot(3, 4, 4)
 plt.imshow(blurredImage, cmap='gray')
 plt.title("Blurred image")
 plt.axis('off')
 
-plt.subplot(2, 3, 5)
+plt.subplot(3, 4, 5)
 plt.imshow(laplaceImage, cmap='gray')
-plt.title("laplace image")
+plt.title("Laplace image")
 plt.axis('off')
 
-plt.subplot(2, 3, 6)
+plt.subplot(3, 4, 6)
 plt.imshow(thresholdedImage, cmap='gray')
 plt.title("Thresholded Image")
 plt.axis('off')
 
-plt.subplot(3,3, 4)
+plt.subplot(3, 4, 7)
 plt.imshow(thresholdedImageBeforeLaplace, cmap='gray')
-plt.title("Thresholded before lapalce Image")
+plt.title("Thresholded before Laplace Image")
 plt.axis('off')
+
+plt.subplot(3, 4, 8)
+plt.imshow(closedImage, cmap='gray')
+plt.title("Closed Image")
+plt.axis('off')
+
+plt.subplot(3, 4, 9)
+plt.imshow(openImage, cmap='gray')
+plt.title("Opened Image")
+plt.axis('off')
+
+plt.subplot(3, 4, 10)  
+plt.imshow(normalized_blurredImage, cmap='gray') 
+plt.title("Normalized Blurred Image")
+plt.axis('off')
+
+circles_detected = circular_hough_transform(blurredImage, radius_range=[20, 100], search_threshold=50)
+image_for_display = cv.cvtColor(openImage, cv.COLOR_GRAY2BGR)
+
+if circles_detected is not None:
+    for circle in circles_detected[0, :]:
+        center = (circle[0], circle[1])  
+        radius = circle[2]  
+        cv.circle(image_for_display, center, radius, (0, 255, 0), 2)  
+        cv.circle(image_for_display, center, 2, (0, 0, 255), 3) 
+        
+print(circles_detected)
+
+
+plt.figure(figsize=(6, 6))
+plt.imshow(cv.cvtColor(image_for_display, cv.COLOR_BGR2RGB))
+plt.title("Image with Detected Circles")
+plt.axis('off')
+plt.show()
 
 plt.tight_layout()
 plt.show()
